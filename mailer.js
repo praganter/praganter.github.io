6799a36fda51953ea4f63b81c13e1ffda639af72
Batuhan Yetgin <byetgin@engineer.com>
2021-12-20 02:30:33 +0300

    Add files via upload

---
+++ mailer.js
@@ -0,0 +1,4 @@
+
+function openMailApp() {
+window.open('mailto:byetgin@engineer.com?subject=Siteden Gelen İletişim İsteği&body=İletişim nedeninizi belirtebilirsiniz.Üşengeçseniz böyle bırakın :D ');
+}
